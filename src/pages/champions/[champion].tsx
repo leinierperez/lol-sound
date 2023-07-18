3aedfc100ebd5289a7f576be753f433c79ee6428
Leinier Perez <P_leinier@yahoo.com>
2023-07-17 23:11:36 -0400

    Add paths for all the champions according to `data.json`

--- src/pages/champions/[champion].tsx
+++ src/pages/champions/[champion].tsx
@@ -1,5 +1,32 @@
-const Champion = () => {
-  return <div>Champion quotes will go here</div>;
-};
+import getChampionData from '@/utils/getChampionData';
+import type { Champion } from '@/types';
 
-export default Champion;
+export async function getStaticProps(context: any) {
+  const data = await getChampionData();
+  const championName = context.params.champion;
+  const championData = data.find(({ name }) => name === championName);
+
+  return {
+    props: {
+      championData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getChampionData();
+  const paths = data.map(({ name }) => {
+    return {
+      params: { champion: name },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Champion({ championData }: { championData: Champion }) {
+  return <div>{championData.name}</div>;
+}
