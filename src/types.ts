bd3449071edf3175fcdfe4ce857bd447d216fa2d
Leinier Perez <P_leinier@yahoo.com>
2023-07-18 22:04:17 -0400

    Add way to get champion data from meraki analytics

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,22 @@ type Quote = {
   quote: string;
   url: string;
 };
+
+export type MerakiChampion = {
+  id: number;
+  key: string;
+  name: string;
+  title: string;
+  icon: string;
+  skins: Skin[];
+};
+
+export type Skin = {
+  name: string;
+  id: number;
+  splashPath: string;
+  uncenteredSplashPath: string;
+  tilePath: string;
+  loadScreenPath: string;
+  loadScreenVintagePath: string | null;
+};
--- src/utils/getChampionData.ts
+++ src/utils/getChampionData.ts
@@ -1,10 +1,30 @@
 import fsPromises from 'fs/promises';
 import path from 'path';
-import type { Champion } from '../types';
+import type { Champion, MerakiChampion } from '../types';
 
 export default async function getChampionData(): Promise<Champion[]> {
   const filePath = path.join(process.cwd(), 'data.json');
   const jsonData = await fsPromises.readFile(filePath, 'utf-8');
   const data = JSON.parse(jsonData) as Champion[];
   return data;
+}
+
+async function getMerakiChampionData(): Promise<MerakiChampion[]> {
+  const response = await fetch(
+    'https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json'
+  );
+  const merakiData: Record<string, MerakiChampion> = await response.json();
+  const merakiChampions: MerakiChampion[] = [];
+  for (const champKey of Object.keys(merakiData)) {
+    const { id, key, name, title, icon, skins } = merakiData[champKey];
+    merakiChampions.push({
+      id,
+      key,
+      name,
+      title,
+      icon,
+      skins,
+    });
+  }
+  return merakiChampions;
 }
