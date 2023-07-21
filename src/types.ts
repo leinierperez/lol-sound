fb53e5e664bd743223434c3960c2453055af2284
Leinier Perez <P_leinier@yahoo.com>
2023-07-20 23:50:38 -0400

    Update data and change types to match

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,17 @@
 export type Quote = {
-  name: string;
-  quotes: QuoteInfo[];
-};
-
-type QuoteInfo = {
   quote: string;
   url: string;
 };
 
-export type Champion = {
-  quotes: QuoteInfo[];
-} & MerakiChampion;
-
-export type MerakiChampion = {
+export interface Champion {
   id: number;
   key: string;
   name: string;
   title: string;
   icon: string;
+  quotes: Quote[];
   skins: Skin[];
-};
+}
 
 export type Skin = {
   name: string;
