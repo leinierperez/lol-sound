a8ddbdc9d881fab60e2d0b24b13bc119e86fa9ec
Leinier Perez <P_leinier@yahoo.com>
2023-07-25 20:36:57 -0400

    Add component that searches the given data and displays the results

---
+++ src/components/SearchableList.tsx
@@ -0,0 +1,32 @@
+import useFuzzySearch from '@/hooks/useFuzzySearch';
+import { Fragment } from 'react';
+
+type Props<T> = {
+  query: string;
+  data: T[];
+  keyToSearch: string;
+  renderItem: (item: T) => React.ReactNode;
+  keyExtractor: (item: T) => string;
+};
+
+function SearchableList<T extends {}>({
+  query,
+  data,
+  renderItem,
+  keyToSearch,
+  keyExtractor,
+}: Props<T>) {
+  const queriedData = useFuzzySearch(query, keyToSearch, data);
+
+  return (
+    <Fragment>
+      {(queriedData.length > 0 || query.length > 0 ? queriedData : data).map(
+        (item) => {
+          return <li key={keyExtractor(item)}>{renderItem(item)}</li>;
+        }
+      )}
+    </Fragment>
+  );
+}
+
+export default SearchableList;
