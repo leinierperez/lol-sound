550677e0b9a8b27172e7f9f3a0af9a758932f1c4
Leinier Perez <P_leinier@yahoo.com>
2023-08-01 19:32:12 -0400

    Add styling

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,28 +1,44 @@
-import Image from 'next/image';
+import { spiegel } from '@/fonts/Spiegel-OTF/Spiegel';
+import { PlayCircle } from 'lucide-react';
 
 type Props = {
   quote: string;
   champName: string;
   champIcon: string;
   quoteAudioURL: string;
+  skin: string;
 };
 
-function Quote({ quote, champName, champIcon, quoteAudioURL }: Props) {
+function Quote({ quote, champName, quoteAudioURL, skin }: Props) {
   return (
-    <div>
-      <div>
-        <Image
-          src={champIcon}
-          width={100}
-          height={100}
-          alt={`${champName}'s icon`}
+    <div
+      className={`bg-slate-900 flex items-center bg-no-repeat bg-cover bg-center p-4 relative h-full rounded-md overflow-hidden z-0`}
+      style={{
+        backgroundImage: `url(${skin})`,
+      }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-60" />
+
+      <div className="z-10 flex items-center gap-4">
+        <PlayCircle
+          size={56}
+          color="white"
+          className="flex-shrink-0 cursor-pointer"
         />
+
+        <div>
+          <p
+            className={`${spiegel.variable} line-clamp-3 text-xl text- font-spiegel`}
+          >
+            {quote}
+          </p>
+          <p
+            className={`${spiegel.variable} text-gray-400 font-spiegel font-semibold tracking-wide`}
+          >
+            {champName}
+          </p>
+        </div>
       </div>
-      <div>
-        <div>{quote}</div>
-        <p>{champName}</p>
-      </div>
-      <audio src={quoteAudioURL} controls />
     </div>
   );
 }
