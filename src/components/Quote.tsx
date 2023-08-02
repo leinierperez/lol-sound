fb73f9219c803a7c436d96bd4e6eb376eb3d675f
Leinier Perez <P_leinier@yahoo.com>
2023-08-01 22:59:55 -0400

    Add lucide-icons and handle the playing of quotes

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,5 +1,6 @@
 import { spiegel } from '@/fonts/Spiegel-OTF/Spiegel';
-import { PlayCircle } from 'lucide-react';
+import useSound from '@/hooks/useSound';
+import { PauseCircle, PlayCircle } from 'lucide-react';
 
 type Props = {
   quote: string;
@@ -10,6 +11,7 @@ type Props = {
 };
 
 function Quote({ quote, champName, quoteAudioURL, skin }: Props) {
+  const { isPlaying, play, pause } = useSound();
   return (
     <div
       className={`bg-slate-900 flex items-center bg-no-repeat bg-cover bg-center p-4 relative h-full rounded-md overflow-hidden z-0`}
@@ -20,12 +22,21 @@ function Quote({ quote, champName, quoteAudioURL, skin }: Props) {
       <div className="absolute inset-0 bg-black bg-opacity-60" />
 
       <div className="z-10 flex items-center gap-4">
-        <PlayCircle
-          size={56}
-          color="white"
-          className="flex-shrink-0 cursor-pointer"
-        />
-
+        {isPlaying ? (
+          <PauseCircle
+            size={56}
+            color="white"
+            className="flex-shrink-0 cursor-pointer"
+            onClick={pause}
+          />
+        ) : (
+          <PlayCircle
+            size={56}
+            color="white"
+            className="flex-shrink-0 cursor-pointer"
+            onClick={() => play(quoteAudioURL)}
+          />
+        )}
         <div>
           <p
             className={`${spiegel.variable} line-clamp-3 text-xl text- font-spiegel`}
