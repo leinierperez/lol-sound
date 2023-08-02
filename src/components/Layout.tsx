e8877754896d7918c46209e624619d1807fa0895
Leinier Perez <P_leinier@yahoo.com>
2023-08-02 15:44:35 -0400

    Remove useless metadata

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,6 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import { Metadata } from 'next';
 import Head from 'next/head';
-
-export const metadata: Metadata = {
-  title: 'League Sounds',
-  description:
-    'This is a meta description. Welcome to slingacademy.com. Happy coding and have a nice day',
-};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
