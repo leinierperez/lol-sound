13bccf818edb0f0ad19f9dd1dc8c565192b03e13
Leinier Perez <P_leinier@yahoo.com>
2023-08-02 19:09:10 -0400

    Remove comments

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,65 +1,7 @@
-// import { spiegel } from '@/fonts/Spiegel-OTF/Spiegel';
-// import useSound from '@/hooks/useSound';
-// import { PauseCircle, PlayCircle } from 'lucide-react';
-
-// type Props = {
-//   quote: string;
-//   champName: string;
-//   champIcon: string;
-//   quoteAudioURL: string;
-//   skin: string;
-// };
-
-// function Quote({ quote, champName, quoteAudioURL, skin }: Props) {
-//   const { isPlaying, play, pause } = useSound();
-//   return (
-//     <div
-//       className={`bg-slate-900 flex items-center bg-no-repeat bg-cover bg-center p-4 relative h-full rounded-md overflow-hidden z-0`}
-//       style={{
-//         backgroundImage: `url(${skin})`,
-//       }}
-//     >
-//       <div className="absolute inset-0 bg-black bg-opacity-60" />
-
-//       <div className="z-10 flex items-center gap-4">
-//         {isPlaying ? (
-//           <PauseCircle
-//             size={56}
-//             color="white"
-//             className="flex-shrink-0 cursor-pointer"
-//             onClick={pause}
-//           />
-//         ) : (
-//           <PlayCircle
-//             size={56}
-//             color="white"
-//             className="flex-shrink-0 cursor-pointer"
-//             onClick={() => play(quoteAudioURL)}
-//           />
-//         )}
-//         <div>
-//           <p
-//             className={`${spiegel.variable} line-clamp-3 text-xl text- font-spiegel`}
-//           >
-//             {quote}
-//           </p>
-//           <p
-//             className={`${spiegel.variable} text-gray-400 font-spiegel font-semibold tracking-wide`}
-//           >
-//             {champName}
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Quote;
-
 import { spiegel } from '@/fonts/Spiegel-OTF/Spiegel';
 import useSound from '@/hooks/useSound';
 import { PauseCircle, PlayCircle } from 'lucide-react';
-import Image from 'next/image'; // Step 1: Import Image from Next.js
+import Image from 'next/image';
 
 type Props = {
   quote: string;
